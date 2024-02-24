3fcc40e8687bbb3a0899ba04f3dd650350bd297a
JvictorVentura <joaovventura@protonmail.com>
2024-02-24 03:03:01 -0400

    kinda working, but with some bugs

--- main.js
+++ main.js
@@ -1,6 +1,9 @@
 comments = document.getElementsByClassName("md");
 comments = Array.from(comments);
 comments = comments.filter( (comment) => comment.innerText.includes("<image>") );
-//comment = comment.map((child) => { child.innerText = child.innerText.replace('<image>', '');
-//                                   child.innerHTML																		
-//																	})
+comments = comments.map((child) => {
+  old = child.innerHTML.match(/<p><a href=\".*\" target=\"_blank\">&lt;image&gt;<\/a><\/p>/g);
+  image_link = child.querySelector('a').getAttribute('href');
+  to_replace = "<img src=" + image_link + " height=50% width=50% >";
+  child.innerHTML = child.innerHTML.replace(old[0], to_replace);
+});
