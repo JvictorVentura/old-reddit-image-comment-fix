ba2c955c49b5c7a71d3a76c9ee9996fcc49bcecb
João Victor R. Ventura <joaovventura@protonmail.com>
2024-08-21 16:25:28 -0400

    fixed more use cases

--- main.js
+++ main.js
@@ -24,49 +24,58 @@ observer.observe(comment_area, observerOptions);
 
 //------------------
 function find_and_replace(){
-  search_for = "<image>"
-  size = '50%'
-  comments = document.getElementsByClassName("md");
-  comments = Array.from(comments);
-  comments = comments.filter( (comment) => comment.innerText.includes(search_for));
+  var size = '50%'
+  var image_tag = "<image>"
+  var url_preview = "https://preview.redd.it"
+  var posts = Array.from(document.getElementsByClassName("md"));
 
-  comments = comments.map( (comment) =>{
-    elements = comment.querySelectorAll('p');
-    elements = Array.from(elements);
-    elements = elements.filter( (element) => element.textContent.includes(search_for));
+  posts = posts.map( (post)=> { return post.querySelectorAll('a')});
+  posts = posts.map( (post)=> { return Array.from(post)});
+  posts = posts.filter( (post)=>post.length > 0);
+  posts = posts.flat();
+  posts = posts.filter( post=> post.getAttribute('href').includes(url_preview));
 
-    elements = elements.map( (element) => {
-      image_link = element.querySelector('a').getAttribute('href');
-      img = document.createElement('img');
-      img.setAttribute('src', image_link);
-      img.setAttribute('height', size);
-      img.setAttribute('width', size);
-      element.querySelector('a').replaceWith(img)
-    })
+  var comments = posts.filter(post => post.innerText.includes(image_tag));
+  comments.map((post) =>{
+        var image_link = post.getAttribute('href');
+        var img = document.createElement('img');
+        img.setAttribute('src', image_link);
+        img.setAttribute('height', size);
+        img.setAttribute('width', size);
+        post.replaceWith(img);
+    });
 
-  })
-
-  //main post
-  comments = document.getElementsByClassName("md");
-  comments = Array.from(comments);
-  comments = comments.filter( (comment) => comment.innerText.includes("https://preview.redd.it"));
-
-  comments.map( (comment) =>{
-    elements = comment.querySelectorAll('p');
-    elements = Array.from(elements);
-    elements = elements.filter( (element) => element.textContent.includes("https://preview.redd.it"));
-
-    elements = elements.map( (element) => {
-      image_link = element.querySelector('a').getAttribute('href');
-      img = document.createElement('img');
-      img.setAttribute('src', image_link);
-      img.setAttribute('height', size);
-      img.setAttribute('width', size);
-      element.querySelector('a').replaceWith(img)
-    })
-})
+  var image_url_on_text = posts.filter( (post)=> post.innerHTML.includes(url_preview));
+  //replace anchors with images
+   image_url_on_text.map((post) =>{
+        var image_link = post.getAttribute('href');
+        var img = document.createElement('img');
+        img.setAttribute('src', image_link);
+        img.setAttribute('height', size);
+        img.setAttribute('width', size);
+        post.replaceWith(img);
+    });
 
 
+  var text_with_url = posts.filter( post=> {
+    if (!post.innerText.includes(image_tag) && !post.innerText.includes(url_preview)) {
+      return true;
+    }else{
+      return false;
+    }
+  });
+  text_with_url.map((post) =>{
+        var image_link = post.getAttribute('href');
+        var text = post.innerText;
+        var paragraph = document.createElement('p');
+        paragraph.innerText = text;
+        var img = document.createElement('img');
+        img.setAttribute('src', image_link);
+        img.setAttribute('height', size);
+        img.setAttribute('width', size);
+        post.insertAdjacentElement('afterend', paragraph);
+        post.replaceWith(img);
+    });
 
 
 }
